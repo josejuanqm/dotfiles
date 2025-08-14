912097653a35260b1fc7f375799560c837fbd7bb
Jose Quintero <jose.juan.qm@gmail.com>
2025-08-14 09:12:18 -0600

    configuration

--- .config/nvim/after/ftplugin/swift.lua
+++
@@ -1,4 +0,0 @@
-local map = vim.api.nvim_set_keymap
-
-map('n', '<leader>bb', '<cmd>:XcodebuildBuild<CR>', { silent = true });
-map('n', '<leader>br', '<cmd>:XcodebuildBuildRun<CR>', { silent = true });
